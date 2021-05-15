11f33399de941115f584804fa48ac98e22aa41b4
SullivanStobo <ss.stobo@gmail.com>
2021-05-15 13:16:41 -0400

    New run cycle and bow draw animation (#27)
    Co-authored-by: Sullivan Stobo <stobos@mcmaster.ca>

--- source/allanly/Player.hx
+++ source/allanly/Player.hx
@@ -47,11 +47,11 @@ class Player extends Character {
     health = 100;
 
     // Images and animations
-    loadGraphic(AssetPaths.player__png, true, 14, 30);
-    animation.add("walk", [0, 1, 2, 3], 10, true);
-    animation.add("idle", [4, 5, 6, 7], 5, true);
-    animation.add("climb", [8, 9, 10, 11], 5, true);
-    animation.add("die", [12, 13, 14, 15, 16], 5, false);
+    loadGraphic(AssetPaths.player__png, true, 23, 30);
+    animation.add("walk", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 20, true);
+    animation.add("idle", [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], 5, true);
+    animation.add("climb", [32], 5, true);
+    animation.add("die", [33], 5, false);
     animation.play("idle");
 
     // Say a little something on creation
