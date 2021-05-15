f340cdd3b97725d7f995e2bb69a80b123ccd3e49
Innovasium <innovasium@Innovasiums-MacBook-Pro.local>
2021-05-15 01:00:35 -0400

    boeuf ba

--- source/allanly/Drone.hx
+++ source/allanly/Drone.hx
@@ -7,15 +7,19 @@ package allanly;
  * 1/6/2015
  */
 // Libraries
+import flixel.FlxG;
 import flixel.math.FlxPoint;
 import flixel.system.FlxSound;
 
 // Swinging enemies
 class Drone extends Character {
+  private static inline final MOVEMENT_SPEED_MAX:Float = 200;
+  private static inline final MOVEMENT_SPEED_CHANGE:Float = 10;
+  private static inline final MOVEMENT_SPEED_DECELERATION_CHANGE:Float = 0.2;
+
   // Pointer to jim
   private var jimPointer:Character;
 
-  // Constants
   private static inline final MOVEMENT_SPEED:Int = 200;
 
   // Create enemy
@@ -43,6 +47,46 @@ class Drone extends Character {
 
   // Move around
   override public function move(elapsed:Float) {
+    ignoreGravity = true;
+    if (FlxG.keys.pressed.LEFT) {
+      // Movement
+      if (velocity.x > -MOVEMENT_SPEED_MAX) {
+        // Less movement acceleration when jumping
+        acceleration.x = -MOVEMENT_SPEED_CHANGE * (MOVEMENT_SPEED_MAX + velocity.x);
+      }
+      // Stop accelerating when we fast
+      else if (velocity.x <= -MOVEMENT_SPEED_MAX) {
+        acceleration.x = 0;
+      }
+    }
+
+    if (FlxG.keys.pressed.RIGHT) {
+      // Movement
+      if (velocity.x < MOVEMENT_SPEED_MAX) {
+        // Less movement acceleration when jumping
+
+        acceleration.x = MOVEMENT_SPEED_CHANGE * (MOVEMENT_SPEED_MAX - velocity.x);
+      }
+      // Stop accelerating when we fast
+      else if (velocity.x >= MOVEMENT_SPEED_MAX) {
+        acceleration.x = 0;
+      }
+    }
+    if (!FlxG.keys.pressed.LEFT && !FlxG.keys.pressed.RIGHT) {
+      // Stopped
+      if (velocity.x < 5 && velocity.x > -5) {
+        acceleration.x = 0;
+        velocity.x = 0;
+      }
+
+      // Decelerating
+      else if (velocity.x > 0) {
+        acceleration.x = -MOVEMENT_SPEED_CHANGE * MOVEMENT_SPEED_DECELERATION_CHANGE * (MOVEMENT_SPEED_MAX + velocity.x);
+      }
+      else if (velocity.x < 0) {
+        acceleration.x = MOVEMENT_SPEED_CHANGE * MOVEMENT_SPEED_DECELERATION_CHANGE * (MOVEMENT_SPEED_MAX - velocity.x);
+      }
+    }
     super.move(elapsed);
   }
 
